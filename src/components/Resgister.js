2277f2ee60a4dddd51c6e76e3ddf02256da1c7fe
Anova Radhakrishnan <68416658+AnovaRK@users.noreply.github.com>
2021-04-07 12:59:25 +0530

    Update Resgister.js

--- src/components/Resgister.js
+++ src/components/Resgister.js
@@ -1,26 +1,53 @@
 import React from 'react';
-import { ScrollView, StyleSheet, TextInput, View, Button, TouchableOpacity, Text } from 'react-native';
+import { ScrollView, StyleSheet, TextInput, View, TouchableOpacity, Text } from 'react-native';
 
 function Register() {
     return (
         <View style={styles.card}>
             <View style={styles.container}>
-                <TextInput style={styles.input} placeholder="Name (As per Co. Records) " />
-                <TextInput style={styles.input} placeholder="Employee Code"/>
-                <TextInput style={styles.input} placeholder="Name of Organiztion" />
-                <TextInput style={styles.input} placeholder="Name of Division"/>
-                <TextInput style={styles.input} placeholder="Designation" />
-                <TextInput style={styles.input} placeholder="Email ID"/>
-                <TextInput style={styles.input} placeholder="City (Company HQ)" />
-                <TextInput style={styles.input} placeholder="Mobile No"/>
-                <TextInput style={styles.input} placeholder="Password" secureTextEntry={true}/>
-                <TouchableOpacity style={styles.btn} >
-                        <Text style={styles.btnText}>SIGN UP</Text>
+                <TextInput style={styles.input} placeholder="Name (As per Co. Records) " 
+                    placeholderTextColor = "grey"
+                    selectionColor="grey"
+                />
+                <TextInput style={styles.input} placeholder="Employee Code" 
+                    placeholderTextColor = "grey"
+                    selectionColor="grey"
+                />
+                <TextInput style={styles.input} placeholder="Name of Organiztion"  
+                    placeholderTextColor = "grey"
+                    selectionColor="grey"
+                />
+                <TextInput style={styles.input} placeholder="Name of Division" 
+                    placeholderTextColor = "grey"
+                    selectionColor="grey"
+                />
+                <TextInput style={styles.input} placeholder="Designation" 
+                    placeholderTextColor = "grey"
+                    selectionColor="grey"
+                />
+                <TextInput style={styles.input} placeholder="Email Address" 
+                    placeholderTextColor = "grey"
+                    selectionColor="grey"
+                />
+                <TextInput style={styles.input} placeholder="City (Company HQ)" 
+                    placeholderTextColor = "grey"
+                    selectionColor="grey"
+                />
+                <TextInput style={styles.input} placeholder="Mobile No" 
+                    placeholderTextColor = "grey"
+                    selectionColor="grey"
+                />
+                <TextInput style={styles.input} placeholder="Password" secureTextEntry={true} 
+                    placeholderTextColor = "grey"
+                    selectionColor="grey"
+                />
+                <TouchableOpacity style={styles.button} >
+                        <Text style={styles.buttonText}>SIGN UP</Text>
                 </TouchableOpacity> 
                 <View style={styles.hyperLinkText}>
                     <Text >Already have account?</Text>
                     <TouchableOpacity >
-                        <Text style={styles.hyperLink}>Create Account</Text>
+                        <Text style={styles.hyperLink}>SIGN IN</Text>
                     </TouchableOpacity>
                 </View>
             </View>
@@ -28,46 +55,38 @@ function Register() {
     );
 }
 const styles = StyleSheet.create({
-    card:{
-        height:650,
-        width:450,
+    container : {
+        flexGrow: 1,
+        justifyContent:'center',
         alignItems: 'center',
-        justifyContent: 'center',
-        backgroundColor: '#fff',
-        margin:0,
-        shadowColor: '#000',
-        shadowOffset: { width: 0, height: 3 },
-        shadowOpacity: 0.5,
-        shadowRadius: 5,  
-        elevation: 5,
-        padding:40,
-    },
-    input:{
-        alignSelf:'stretch',
-        color: '#000',
-        height: 40,
-        width:350,
-        marginBottom: 10,
-        borderBottomColor: '#dad9d9',
+        marginVertical:120,
+      },
+      input: {
+        paddingLeft:15,
+        paddingBottom:10,
+        paddingTop:10,
+        width:310,
+        fontSize:16,
+        color:'#000',
+        marginVertical: 5,
+        marginBottom: 5,
+        borderBottomColor:'#dad9d9',
         borderBottomWidth: 1,
-        padding:5,
-    },
-    btn:{
-        backgroundColor: '#a40b0e',
-        
-        width:350,
-        height:50,
-        borderColor:'#a40b0e',
-        alignItems: 'center',
-        justifyContent: 'center',
-        borderRadius:25,
-        marginVertical:16,
-    },
-    btnText:{
-        color: '#fff',
-    },
+      },
+      button: {
+        width:310,
+        backgroundColor:'#a40b0e',
+        borderRadius: 25,
+        marginVertical: 5,
+        paddingVertical: 13
+      },
+      buttonText: {
+        fontSize:16,
+        fontWeight:'500',
+        color:'#ffffff',
+        textAlign:'center'
+      },
     hyperLinkText:{
-        flexGrow:1,
         alignItems:'center',
         fontSize:16,
         marginVertical:16,
